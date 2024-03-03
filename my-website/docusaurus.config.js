291b2aca8ebf45d30a84ef20cf9d7dedc7e6fbf8
eduesplinio <eduesplinio.dat@gmail.com>
2024-03-02 22:55:52 -0300

    fix: altera `docusaurus.config.js`

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -7,15 +7,10 @@ const config = {
   favicon: 'img/eduTechExp-logo.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://edu-tech-exp.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
