117f802332d864f94de2b13590982ea73c50f475
eduesplinio <eduesplinio.dat@gmail.com>
2024-03-05 20:13:32 -0300

    fix: update docusaurus config and sidebar

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   url: 'https://edu-tech-exp-ten.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
