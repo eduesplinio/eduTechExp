898f2897643e4cf2910cd47529298033141df72b
eduesplinio <eduesplinio.dat@gmail.com>
2024-03-05 19:33:26 -0300

    fix: edita `URL` para Vercel

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -7,7 +7,7 @@ const config = {
   favicon: 'img/eduTechExp-logo.ico',
 
   // Set the production url of your site here
-  url: 'https://edu-tech-exp.netlify.app',
+  url: 'edu-tech-exp.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
