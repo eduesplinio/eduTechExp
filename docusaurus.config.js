80ebb56e40c668e567a3b80c71794a718f337a65
eduesplinio <eduesplinio.dat@gmail.com>
2024-03-22 10:16:26 -0300

    adiciona `baseURL`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   url: 'https://edutechexp.me                                       ',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '',
+  baseUrl: '/eduTechExp',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
